86f8f8f7eececdb870302440761df381e6503dc2
webwerks1 <webwerks1@scrubadoo.com>
2019-05-31 17:52:07 +0530

    built rest api that stores the todo list in array

---
+++ app.js
@@ -0,0 +1,44 @@
+import createError from 'http-errors';
+import express, { json, urlencoded, static as express_static } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+// Routes
+import todoRouter from './routes/todo';
+
+var app = express();
+
+// view engine setup
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express_static(join(__dirname, 'public')));
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', todoRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
